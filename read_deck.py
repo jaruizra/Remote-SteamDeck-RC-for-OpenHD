b2df99ae844f8a5896f9b745b5471b9e1b441e03
jaruizra <rura.javier@gmail.com>
2025-03-07 06:11:29 +0100

    test

--- read_deck.py
+++ read_deck.py
@@ -2,10 +2,4 @@ from inputs import devices
 
 for device in devices.gamepads:
     print("Device Name:", device.name)
-    # Try accessing an attribute named 'path' (if it exists)
-    device_path = getattr(device, 'path', None)
-    if device_path is not None:
-        print("Device Path:", device_path)
-    else:
-        print("Device Path attribute not available.")
-    print("Capabilities:", device.capabilities())
+    print("Available attributes and methods:", dir(device))
