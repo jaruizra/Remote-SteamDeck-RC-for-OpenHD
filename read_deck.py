3a29a2ac6da775b2204521847b46e25079a93d11
jaruizra <rura.javier@gmail.com>
2025-03-07 05:46:24 +0100

    test

--- read_deck.py
+++ read_deck.py
@@ -1,6 +1,9 @@
 from inputs import get_gamepad
 
 while True:
+    print("1")
     events = get_gamepad()  # Blocks until an event is available
+    print("1")
     for event in events:
+        print("1")
         print(f"Type: {event.ev_type}, Code: {event.code}, State: {event.state}")
