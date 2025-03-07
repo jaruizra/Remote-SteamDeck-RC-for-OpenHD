ddf51b14e61cd241c167e0e77169567165ccd2c5
jaruizra <rura.javier@gmail.com>
2025-03-07 06:16:58 +0100

    test

--- read_deck.py
+++ read_deck.py
@@ -1,5 +1,14 @@
 from inputs import devices
+from inputs import get_gamepad
+
 
 for device in devices.gamepads:
     print("Device Name:", device.name)
     print("Available attributes and methods:", dir(device))
+
+print("!")
+while True:
+    events = get_gamepad()  # This waits for events from any gamepad device
+    print("!")
+    for event in events:
+        print(f"Type: {event.ev_type}, Code: {event.code}, State: {event.state}")
