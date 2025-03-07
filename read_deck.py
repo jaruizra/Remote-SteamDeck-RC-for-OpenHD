cb9919de0161329a5da591835fb765e62b3693e8
jaruizra <rura.javier@gmail.com>
2025-03-07 05:35:01 +0100

    test

--- read_deck.py
+++ read_deck.py
@@ -1,9 +1,16 @@
 import pygame
 
-
 pygame.joystick.init()
-joysticks = [pygame.joystick.Joystick(x) for x in range(pygame.joystick.get_count())]
 
-print(joysticks)
+# Get a list of joystick objects
+joysticks = [pygame.joystick.Joystick(i) for i in range(pygame.joystick.get_count())]
 
-print(joysticks.get_instadnce_id())
+print("Available joysticks:", joysticks)
+
+if joysticks:
+    # For example, use the first joystick
+    joystick = joysticks[0]
+    joystick.init()  # Ensure the joystick is initialized
+    print("Joystick instance ID:", joystick.get_instance_id())
+else:
+    print("No joysticks found.")
